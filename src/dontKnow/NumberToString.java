3c4cf61c7ad73916eb0e27f9619e91a7ceea75de
SRINIVAS REDDY GOURU <sgouru@sgouru-macbookpro.roam.corp.google.com>
2019-04-06 19:43:33 +0530

    Program to print the possible strings from given number

---
+++ src/dontKnow/NumberToString.java
@@ -0,0 +1,87 @@
+package dontKnow;
+
+
+/*
+ * 
+ * This program is to print the possible strings from given number.
+ * 
+ * Conditions:
+ * 	If number consists of continuous Zeros, program gives runtime exception.
+ * 	
+ * 	Still we can optimize this program by using character array instead of string and
+ * 	pass start and end indexes as function parameters. Substring operations are costly.
+ * 
+ * 	NOTE: This program prints all permutations, i.e. repeated values as well. We can use HashMap or Set to overcome this.
+ * 
+ */
+
+public class NumberToString {
+
+	public String alphabets;
+	
+	public NumberToString()
+	{
+		alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	}
+	
+	
+	public void printString(String number)
+	{
+		printStringHelper(number, "");
+	}
+
+	private void printStringHelper(String number, String prefix) {
+		
+		if(number.equals(""))
+		{
+			System.out.println(prefix);
+		}
+		else
+		{
+			Integer i =Integer.parseInt(number.charAt(number.length()-1)+"");
+			Integer ii = -1;
+			if(number.length() >= 2)
+			{
+				ii = Integer.parseInt(""+number.charAt(number.length()-2)+number.charAt(number.length()-1));
+			}
+			if(i != 0 )
+			{
+				printStringHelper(number.substring(0,number.length()-1),alphabets.charAt(i-1) +prefix);
+			}
+			
+			if(ii == 0)
+			{
+				System.out.println("Not posible..");
+				throw new RuntimeException("Not possible to create a string");
+			}
+			
+			else if(( i == 0 && ii != -1) || 
+					(ii != -1 && ii <= 26 )
+					)
+			{
+				printStringHelper(number.substring(0,number.length()-2),alphabets.charAt(ii-1) +prefix);
+			}
+					
+			
+			
+		}
+		
+		
+	}
+
+	public static void main(String args[])
+	{
+		NumberToString ns = new NumberToString();
+		
+//		ns.printString("123");
+//		ns.printString("314");
+//		ns.printString("12321");
+//		ns.printString("100"); // throws runtime exception
+//		ns.printString("1232");
+//		ns.printString("123210");
+		ns.printString("01");
+		
+	}
+	
+	
+}
