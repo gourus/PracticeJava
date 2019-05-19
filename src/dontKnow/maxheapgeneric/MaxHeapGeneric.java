f56aa7215889448b57f5758820f0082f8bb08839
SRINIVAS REDDY GOURU <sgouru@sgouru-macbookpro.roam.corp.google.com>
2019-05-19 18:18:18 +0530

    Format

--- src/dontKnow/maxheapgeneric/MaxHeapGeneric.java
+++ src/dontKnow/maxheapgeneric/MaxHeapGeneric.java
@@ -1,12 +1,11 @@
 package dontKnow.maxheapgeneric;
 
-
 public class MaxHeapGeneric<E extends Comparable<E>> {
 
-	private E [] heap;
+	private E[] heap;
 	private int size;
-	private int maxSize; 
-	
+	private int maxSize;
+
 	public Object[] getHeap() {
 		return heap;
 	}
@@ -31,124 +30,103 @@ public class MaxHeapGeneric<E extends Comparable<E>> {
 		this.maxSize = maxSize;
 	}
 
-	public MaxHeapGeneric(int size)
-	{
-		this.heap = (E[]) new Comparable[size+1];
+	@SuppressWarnings("unchecked")
+	public MaxHeapGeneric(int size) {
+		this.heap = (E[]) new Comparable[size + 1];
 		this.maxSize = size;
 		this.size = 0;
 	}
-	
-	private void swap(int i, int j)
-	{
+
+	private void swap(int i, int j) {
 		E temp = heap[i];
 		heap[i] = heap[j];
 		heap[j] = temp;
-		
+
 	}
-	
-	private int parent(int i)
-	{
-		return (i-1)/2;
+
+	private int parent(int i) {
+		return (i - 1) / 2;
 	}
-	
-	
-	private int left(int i)
-	{
-		return 2*i+1;
+
+	private int left(int i) {
+		return 2 * i + 1;
 	}
-	
-	private int right(int i)
-	{
-		return i*i +2;
+
+	private int right(int i) {
+		return i * i + 2;
 	}
-	
-	//Insert.
-	//Insert last and compare with parent.
+
+	// Insert.
+	// Insert last and compare with parent.
 	// If it is greater, then swap and do the same until current == root
 	// NOTE: For MAX_HEAP
-	
-	public void insert(E value)
-	{
-		
-		if(size == maxSize)
-		{
+
+	public void insert(E value) {
+
+		if (size == maxSize) {
 			throw new RuntimeException("Heap is full...");
 		}
 
-		//Comparable<? super E> key;
-		
-		heap[size] = value;
-		
-		int current = size;
-		
-		while(current > 0 )
-		{
-			//key = (Comparable<? super E>)heap[current];
+		// Comparable<? super E> key;
 
-			if( heap[current].compareTo( heap[parent(current)]) > 0)
-			{
-				swap(current , parent(current));
-				current = parent(current);
-			}
-			else
-			{
-				break;
-			}
+		heap[size] = value;
+
+		int current = size;
+
+		while (current > 0 && (heap[current].compareTo(heap[parent(current)]) > 0)) {
+			// key = (Comparable<? super E>)heap[current];
+			swap(current, parent(current));
+			current = parent(current);
 
 		}
-		
+
 		size++;
-		
+
 	}
-	
+
 	// Heapify the value from given index
-	private void maxHeapify( int i)
-	{
+	private void maxHeapify(int i) {
 		int l = left(i);
 		int r = right(i);
-		//Comparable<? super E> key;
-		
+		// Comparable<? super E> key;
+
 		int largest = i;
-		
-		//key = (Comparable<? super E>)heap[largest];
-		
-		if( l < size  && heap[largest].compareTo( heap[l]) < 0 )
-		{	
+
+		// key = (Comparable<? super E>)heap[largest];
+
+		if (l < size && heap[largest].compareTo(heap[l]) < 0) {
 			largest = l;
 		}
 
-		//key = (Comparable<? super E>)heap[largest];
-		
-		if(r < size && heap[largest].compareTo((E) heap[r]) < 0)
-		{
-			
+		// key = (Comparable<? super E>)heap[largest];
+
+		if (r < size && heap[largest].compareTo((E) heap[r]) < 0) {
+
 			largest = r;
 		}
-		
-		if(largest != i)
-		{
-			swap(largest,i);
-			
+
+		if (largest != i) {
+			swap(largest, i);
+
 			maxHeapify(largest);
 		}
 	}
-	
+
 	// Extract max value from heap
 	// assign last value to root value and heapify root
-	public E extractMax()
-	{
+	public E extractMax() {
 		E res = heap[0];
-		
-		heap[0] = heap[size-1];
+
+		heap[0] = heap[size - 1];
 		size--;
 		maxHeapify(0);
 		return res;
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		MaxHeapGeneric<Integer> maxHeap = new MaxHeapGeneric<Integer>(10);
-		
+
 		maxHeap.insert(10);
 		maxHeap.insert(3);
 		maxHeap.insert(55);
@@ -158,33 +136,26 @@ public class MaxHeapGeneric<E extends Comparable<E>> {
 		maxHeap.insert(0);
 		maxHeap.insert(122);
 		maxHeap.insert(9);
-		
-		for(int i=0; i<9; i++ )
-		{
+
+		for (int i = 0; i < 9; i++) {
 			System.out.print(maxHeap.extractMax() + " ");
 		}
-		
+
 		System.out.println();
-	
-		
+
 		MaxHeapGeneric<Key> keyMaxHeap = new MaxHeapGeneric<Key>(3);
-		
+
 		// Testing with Key class
 		keyMaxHeap.insert(new Key('a', 3));
-		keyMaxHeap.insert(new Key('b',1));
-		keyMaxHeap.insert(new Key('c',1));
-		
-		
-		for(int i=0; i<3; i++ )
-		{
+		keyMaxHeap.insert(new Key('b', 1));
+		keyMaxHeap.insert(new Key('c', 1));
+
+		for (int i = 0; i < 3; i++) {
 			System.out.print(keyMaxHeap.extractMax());
 		}
-		
+
 		System.out.println();
-		
-		
+
 	}
-		
-		
-	
+
 }
